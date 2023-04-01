59626fbd320edb252a7fe7ae798229153ce2f495
nktp <narenkarthicktp@gmail.com>
2023-04-02 00:09:36 +0530

    1,2,3,5,6 cleared

--- src/main.py
+++ src/main.py
@@ -1,37 +1,25 @@
 from reader import read_source
 
+# # Milestone 1
+# template = read_source("Milestone_Input/Milestone 1/Format_Source.txt")
+# template[0].record_polygon("results/Milestone 1.txt")
+# template[1].record_polygon("results/Milestone 1.txt")
+# exit(0)
 
-M1 = "Milestone_Input/Milestone 1/Format_Source.txt"
-R1 = "results/Milestone 1.txt"
-M2 = "Milestone_Input/Milestone 2/Source.txt"
-P2 = "Milestone_Input/Milestone 2/POI.txt"
-R2 = "results/Milestone 2.txt"
-M3 = "Milestone_Input/Milestone 3/Source.txt"
-P3 = "Milestone_Input/Milestone 3/POI.txt"
-R3 = "results/Milestone 3.txt"
-M4 = "Milestone_Input/Milestone 4/Source.txt"
-P4 = "Milestone_Input/Milestone 4/POI.txt"
-R4 = "results/Milestone 4.txt"
-M5 = "Milestone_Input/Milestone 5/Source.txt"
-P5 = "Milestone_Input/Milestone 5/POI.txt"
-R5 = "results/Milestone 5.txt"
-M6 = "Milestone_Input/Milestone 6/Source.txt"
-P6 = "Milestone_Input/Milestone 6/POI.txt"
-R6 = "results/Milestone 6.txt"
-M7 = "Milestone_Input/Milestone 7/Source.txt"
-P7 = "Milestone_Input/Milestone 7/POI.txt"
-R7 = "results/Milestone 7.txt"
+# Other Milestones
+N = 4
+SEARCH_AREA = f"Milestone_Input/Milestone {N}/Source.txt"
+TEMPLATE = f"Milestone_Input/Milestone {N}/POI.txt"
+RESULTS = f"results/Milestone {N}.txt"
 
-M,P,R = M7,P7,R7
+search_area_polygons = read_source(SEARCH_AREA)
+template_polygons = read_source(TEMPLATE)
 
-template = read_source(M)
-pois = read_source(P)
+for polygon in search_area_polygons:
 
-for polygon in template:
-	match_flag = True
-	for poi in pois:
-		if not polygon.is_match(poi):
-			match_flag = False
+	for template_polygon in template_polygons:
+		if template_polygon.foo(polygon):
+			polygon.record_polygon(RESULTS)
 			break
-	if match_flag:
-		polygon.record_polygon(R)
+
+# Milestones passed : 1,2,3,5,6
--- src/polygon.py
+++ src/polygon.py
@@ -1,14 +1,7 @@
-# utility functions to rotate co-ordinates
-# def rotate_90(xy):
-#     return ( -xy[1], xy[0])
-# def rotate_180(xy):
-#     return ( -xy[0], -xy[1])
-# def rotate_270(xy):
-#     return ( xy[1], -xy[0])
-
-class polygon(object):
+class polygon():
 	"""Represents an individual polygon"""
 	def __init__( self, meta, xy):
+
 		self.meta = meta
 		self.xy = []
 		_,nop,*points = xy.split("  ")
@@ -17,6 +10,7 @@ class polygon(object):
 			self.xy.append(tuple(map( int, point.split(' '))))
 
 	def display_polygon(self):
+
 		print(self.meta,end='')
 		print("xy",end='  ')
 		print(self.nop,end='  ')
@@ -25,6 +19,7 @@ class polygon(object):
 		print('\b\nendel')
 
 	def record_polygon( self, filename):
+
 		with open(filename, 'a') as f:
 			f.write(self.meta)
 			f.write("xy  ")
@@ -33,67 +28,24 @@ class polygon(object):
 				f.write("  "+str(i[0])+' '+str(i[1]))
 			f.write("\nendel\n")
 
-	def is_match_3( self, target):
-		# if no. of points doesnt match, not the same polygon
-		if self.nop != target.nop:
-			return False
-		# different approach checking for the offset od the polygons
-		import math
-		offset = math.dist( self.xy[0], target.xy[0])
-		for i,j in zip( self.xy, target.xy):
-			if offset != math.dist( i, j):
-				return False
-		return True
+	def foo( self, target):
 
-	def is_match_2( self, target):
-		# different approach
 		# if no. of points doesnt match, not the same polygon
 		if self.nop != target.nop:
 			return False
 
+		# if length of all sides are equal
 		import math
-		p1 = p2 = 0
-		# finding the first side of the second polygon that matches with the first polygon and the cycling through to check if they are same
-		while p1 < self.nop-2:
-			if math.dist( self.xy[p1], self.xy[p1+1]) != math.dist( target.xy[p2], target.xy[p2+1]):
-				if p1 != 0:
-					return False
-				else:
-					p2 += 1
-					if p2 == target.nop-2:
-						return False
-			else:
-				p1 += 1
-				p2 += 1
-			# if p2 >= target.nop-1:
-				# p2 = 0
-			p2 = p2%(target.nop-1)
-		return True
+		self_sides = []
+		target_sides = []
+		for i in range(self.nop-1):
+			self_sides.append( math.dist(self.xy[i],self.xy[i+1]))
+			target_sides.append( math.dist(target.xy[i],target.xy[i+1]))
 
-	def is_match( self, target):
-		# if no. of points doesnt match, not the same polygon
-		if self.nop != target.nop:
-			return False
+		# Only for milestone 3 n 5
+		# self_sides.sort()
+		# target_sides.sort()
 
-		import math
-		# checking if the sides are all equal
-		shape1 = shape2 = []
-		for i in range(self.nop-2):
-			shape1.append(math.dist( self.xy[i], self.xy[i+1]))
-			shape2.append(math.dist( target.xy[i], target.xy[i+1]))
-		shape1.sort()
-		shape2.sort()
-		return shape1 == shape2
-
-	def merge_polygons( self, polygon2):
-
-		newcoords = []
-		if polygon2.xy == self.xy:
+		if self_sides == target_sides:
 			return True
-		else:
-			for i,j in zip(self.xy,polygon2.xy):
-				newcoords.append(i)
-				if i != j:
-					newcoords.append(j)
-
-		return newcoords
+		return False
